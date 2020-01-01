56319d5126f57e0196412335c68f4ca7b9d4af98
zsibbaszto@gmail.com <zsibbaszto@gmail.com>
2020-01-01 19:00:38 +0000

    replaced

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-print("hello te kutya")
-print ("another kutya")
-=======
 print("hello te kutya te nagon")
->>>>>>> ba67217f1a8c2092dd5b6d6312f45d1de9197793
+print ("another kutya")
+
+
